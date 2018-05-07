07cf9339c3c9bf5aee26cac1c7cffccba9b5b161
Mufaddal Motorwala <mufaddal.motorwala@rbbideas.com>
2018-05-07 18:22:18 +0400

    WIP

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import ReduxPromise from 'redux-promise'
 import App from './components/app';
 import reducers from './reducers';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -13,3 +14,4 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.querySelector('.container'));
+
